01e356c489b07d747a09c5c2a9da8e707439aee5
AK <kkucenko.aleksejs@gmail.com>
2022-04-28 16:05:44 +0300

    add cors to processing

--- qk-processing/src/main.ts
+++ qk-processing/src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
@@ -6,6 +7,9 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  
+  const configService = app.get(ConfigService);
+  app.enableCors({ origin: configService.get<string>("FRONTEND_URL") });
 
   await app.listen(process.env.PROCESSING_PORT);
 }
