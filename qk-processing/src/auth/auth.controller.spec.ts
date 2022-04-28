ee961a192c43d2761b7d1e851e01e4ab09d4f010
AK <kkucenko.aleksejs@gmail.com>
2022-04-29 02:56:25 +0300

    start auth service unit test

--- qk-processing/src/auth/auth.controller.spec.ts
+++ qk-processing/src/auth/auth.controller.spec.ts
@@ -5,22 +5,28 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
+type RegisterUser = {
+    uuid: string
+    email: string
+    createdAt: Date
+}
+
 describe("AuthController Unit Test", () => {
   let controller: AuthController;
 
   const mockAuthService = {
-    login: jest.fn(():string => {
+    login: jest.fn((): string => {
       return "/route-to-be-redirected";
     }),
     register: jest.fn(() => {
       return {
         uuid: "uuid",
         email: "email@email.com",
-        createdAt: new Date(),
+        createdAt: new Date(1651188244),
       };
     }),
   };
-  
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
@@ -29,7 +35,7 @@ describe("AuthController Unit Test", () => {
       .overrideProvider(AuthService)
       .useValue(mockAuthService)
       .compile();
-    
+
     controller = module.get<AuthController>(AuthController);
   });
 
@@ -37,20 +43,30 @@ describe("AuthController Unit Test", () => {
     expect(controller).toBeDefined();
   });
 
-  it("Should register a user", () => {
-    const dto = new AuthDto;
-    const user: { uuid: string, email: string, createdAt: Date } = {
+  describe("/register endpoint - unit", () => {
+    const newUser: AuthDto = {
+      email: "email@email.com",
+      password: "123",
+    };
+    const registeredUser: RegisterUser = {
       uuid: "uuid",
       email: "email@email.com",
-      createdAt: new Date(),
+      createdAt: new Date(1651188244),
     };
-    expect(controller.register(dto)).toEqual(user);
-    expect(mockAuthService.register).toHaveBeenCalledTimes(1);
+    it("Should register a user", () => {
+      expect(controller.register(newUser)).toEqual(registeredUser);
+      expect(mockAuthService.register).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it("Should login a user", () => {
-    const dto = new AuthDto;
-    expect(controller.login(dto, response)).toEqual("/route-to-be-redirected");
-    expect(mockAuthService.login).toHaveBeenCalledTimes(1);
+  describe("/login endpoint - unit", () => {
+    const existentUser: AuthDto = {
+      email: "email@email.com",
+      password: "123",
+    };
+    it("Should login a user", () => {
+      expect(controller.login(existentUser, response)).toEqual("/route-to-be-redirected");
+      expect(mockAuthService.login).toHaveBeenCalledTimes(1);
+    });
   });
 });
