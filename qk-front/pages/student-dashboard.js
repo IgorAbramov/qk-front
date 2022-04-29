f5d5a667c811b5407d52a626f2749b3f44847900
AK <kkucenko.aleksejs@gmail.com>
2022-04-29 14:53:17 +0300

    make nextjs server side request to processing container

--- qk-front/pages/student-dashboard.js
+++ qk-front/pages/student-dashboard.js
@@ -1,5 +1,22 @@
+import getConfig from "next/config"
+
 import Heading from "../components/UI/Heading/Heading"
 
-export default function StudentDashboard() {
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+const apiUrl = serverRuntimeConfig.apiUrl || publicRuntimeConfig.apiUrl
+
+export default function StudentDashboard({ json }) {
+   console.log(json)
    return <Heading blue h2>Student dashboard page</Heading>
+}
+
+export const getServerSideProps = async () => {
+   try {
+      const res = await fetch(`${apiUrl}/auth/test1`)
+      const json = await res.json()
+      return { props: { json } }
+   } catch (error) {
+      return { notFound: true }
+   }
 }
