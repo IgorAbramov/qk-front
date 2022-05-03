421dc2219c17b5971ee22246b586ebf8b4b97e6c
AK <kkucenko.aleksejs@gmail.com>
2022-05-04 00:50:06 +0300

    save file to server --start--

--- qk-front/components/UI/Modal/FileUploadModal/FileUploadModal.jsx
+++ qk-front/components/UI/Modal/FileUploadModal/FileUploadModal.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
+import axios from "axios"
 import { useRecoilState } from "recoil"
 
 import { uploadModalState } from "../../../../atoms/uploadModalState"
+import Button from "../../Button/Button"
 import FileUploadDropdown from "../../Dropdown/FileUploadDropdown/FileUploadDropdown"
 import Heading from "../../Heading/Heading"
 import Input from "../../Input/Input"
@@ -12,6 +14,16 @@ import styles from "./FileUploadModal.module.scss"
 const FileUploadModal = () => {
 
    const [openModal, setOpenModal] = useRecoilState(uploadModalState)
+
+   const uploadFileToClient = e => {
+      if (e.target.files && e.target.files[0]) {
+         const formData = new FormData()
+         formData.append("file", e.target.files[0])
+         axios.post("/api/upload", formData,{ headers: { "Content-Type": "mulitpart/form-data" } })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+      }
+   }
 
    const outsideClickRef = useRef()
    useEffect(() => {
@@ -46,8 +58,9 @@ const FileUploadModal = () => {
                   <Heading blue h2 modal>Multi-Upload</Heading>
                   <Text modal>You have confirmed the list of credentials
                      are ready to be authenticated.</Text>
-                  <Input fileUpload inputName="csvUploader"/>
+                  <Input fileUpload inputName="csvUploader" onChange={uploadFileToClient}/>
                </div>
+               {/*TODO: Behaviour before file upload - empty screen.*/}
                <div className={styles.middle}>
                   {["info", "ksdajfhsj", "sadfasdfas", "sadfsda", "asdfsadfkjsah"].map(test => (
                      <div key={test} className={styles.row}>
@@ -58,6 +71,22 @@ const FileUploadModal = () => {
                      </div>
                   ))}
                </div>
+               <Button blue>
+                  <svg fill="none" height="32" viewBox="0 0 32 32"
+                       width="32" xmlns="http://www.w3.org/2000/svg">
+                     <path d="M12 26H9C7.14348 26 5.36301 25.2625 4.05025 23.9497C2.7375 22.637 2 20.8565 2 19C2 17.1435 2.7375 15.363 4.05025 14.0503C5.36301 12.7375 7.14348 12 9 12C9.58566 11.9998 10.1692 12.0711 10.7375 12.2125"
+                        stroke="white" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+                     <path d="M10 16C10 14.4155 10.3765 12.8536 11.0986 11.4432C11.8206 10.0327 12.8675 8.81406 14.1529 7.88758C15.4383 6.96109 16.9255 6.35333 18.4919 6.11437C20.0583 5.87541 21.6591 6.0121 23.1623 6.51317C24.6655 7.01424 26.0281 7.86534 27.1378 8.99635C28.2476 10.1274 29.0727 11.5059 29.5451 13.0183C30.0176 14.5308 30.1239 16.1338 29.8552 17.6954C29.5866 19.257 28.9507 20.7324 28 22"
+                        stroke="white" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+                     <path d="M14.7617 20.2375L18.9992 16L23.2367 20.2375" stroke="white" strokeLinecap="round"
+                           strokeLinejoin="round" strokeWidth="1.5"/>
+                     <path d="M19 26V16" stroke="white" strokeLinecap="round"
+                           strokeLinejoin="round" strokeWidth="1.5"/>
+                  </svg>
+                  <span>Upload Now</span>
+               </Button>
             </div>
          </div>
       </div>
