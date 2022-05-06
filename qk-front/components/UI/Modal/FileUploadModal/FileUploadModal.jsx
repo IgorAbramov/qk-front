44e3593f34465dc4bfaa76886502b8825aec7cf3
AK <kkucenko.aleksejs@gmail.com>
2022-05-06 21:47:35 +0300

    correct data send to processing and correct state in ui

--- qk-front/components/UI/Modal/FileUploadModal/FileUploadModal.jsx
+++ qk-front/components/UI/Modal/FileUploadModal/FileUploadModal.jsx
@@ -4,7 +4,7 @@ import axios from "axios"
 import { useRecoilState, useResetRecoilState } from "recoil"
 
 import { credentialsState, currentFileState, dropdownSelectionListenerState, filenameState, filePrefixState, fileUploadErrorState, uploadModalState } from "../../../../atoms"
-import { validateMappingFields } from "../../../../utils"
+import { processingUrl, validateMappingFields } from "../../../../utils"
 import Button from "../../Button/Button"
 import FileUploadDropdown from "../../Dropdown/FileUploadDropdown/FileUploadDropdown"
 import Heading from "../../Heading/Heading"
@@ -28,8 +28,7 @@ const FileUploadModal = () => {
    const [fileName, setFileName] = useRecoilState(filenameState)
    const [parsedValuesFromUpload, setParsedValuesFromUpload] = useState([])
    const [mappingToValues, setMappingToValues] = useState([])
-
-   console.log(credentialsFields)
+   const [uploadSuccess, setUploadSuccess] = useState(false)
 
    const uploadFileToClient = async e => {
       if (e.target.files[0]?.type !== "text/csv") {
@@ -73,30 +72,37 @@ const FileUploadModal = () => {
       const arrayOfValues = mappingToValues.map(mapping => mapping?.value)
       const validation = validateMappingFields(arrayOfValues)
 
-      if (validation) {
+      if (!validation) {
          setFileUploadModalError("")
          const mapping = mappingToValues.map(mapping => {
             return mapping?.value
          }).join(",")
-         const blob = new Blob([mapping], { type: "text/plain" })
          const formData = new FormData()
-         formData.append("parsedFile", currentFile)
-         formData.append("data", blob)
+         formData.append("file", currentFile)
+         formData.append("mapping", mapping)
          console.log(mapping)
          console.log([...formData])
 
          resetCurrentFile()
 
-         // axios.post(`${processingUrl}/uploads`, formData)
-         //    .then(res => console.log(res))
-         //    .catch(err => console.log(err))
+         axios.post(`${processingUrl}/upload`, formData, { withCredentials: true })
+            .then(res => {
+               if (res.status === 201) {
+                  console.log(res)
+                  setUploadSuccess(true)
+               }
+            })
+            .catch(err => {
+               console.log(err)
+               setFileUploadModalError(err.response.statusText)
+            })
 
          const data = JSON.stringify(`${filePrefix}-${fileName}`)
          axios.post("api/file-delete", data, { headers: { "Content-type": "application/json" } })
             .then(res => {
                console.log(res)
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log(err, "ERROR ?"))
       } else {
          setFileUploadModalError("Please, choose all required fields")
       }
@@ -137,66 +143,90 @@ const FileUploadModal = () => {
    }, [openModal])
 
    return (
-      <div className={styles.modal}>
-         <div ref={outsideClickRef} className={styles.wrapper}
-              style={{ height: parsedValuesFromUpload.length ? "90%" : "" }}>
-            <svg className={styles.close} fill="none" height="46"
-                 viewBox="0 0 46 46"
-                 width="46"
-                 xmlns="http://www.w3.org/2000/svg" onClick={closeModal}>
-               <path d="M31.1424 31.4853C35.8287 26.799 35.8287 19.201 31.1424 14.5147C26.4561 9.82843 18.8581 9.82843 14.1718 14.5147C9.48551 19.201 9.48551 26.799 14.1718 31.4853C18.8581 36.1716 26.4561 36.1716 31.1424 31.4853Z"
-                  stroke="#737373" strokeLinecap="round"
-                  strokeLinejoin="round" strokeWidth="1.5"/>
-               <path d="M19.1211 26.5356L26.1922 19.4646" stroke="#737373" strokeLinecap="round"
-                     strokeLinejoin="round" strokeWidth="1.5"/>
-               <path d="M19.1211 19.4644L26.1922 26.5354" stroke="#737373" strokeLinecap="round"
-                     strokeLinejoin="round" strokeWidth="1.5"/>
-            </svg>
-            <div className={styles.wrapperInner} style={{ height: parsedValuesFromUpload.length ? "100%" : "" }}>
-               <div className={styles.top}>
-                  <Heading blue h2 modal>Multi-Upload</Heading>
-                  <Text modal>You have confirmed the list of credentials
-                     are ready to be authenticated.</Text>
-                  <Input fileUpload fileName={fileName} inputName="csvUploader"
-                         isFileUploaded={!!parsedValuesFromUpload.length}
-                         onChange={uploadFileToClient}/>
-                  {fileUploadModalError && <Text error>{fileUploadModalError}</Text>}
+      uploadSuccess
+         ? <div className={styles.modal}>
+            <div ref={outsideClickRef} className={styles.wrapper}>
+               <svg className={styles.close} fill="none" height="46"
+                    viewBox="0 0 46 46"
+                    width="46"
+                    xmlns="http://www.w3.org/2000/svg" onClick={closeModal}>
+                  <path d="M31.1424 31.4853C35.8287 26.799 35.8287 19.201 31.1424 14.5147C26.4561 9.82843 18.8581 9.82843 14.1718 14.5147C9.48551 19.201 9.48551 26.799 14.1718 31.4853C18.8581 36.1716 26.4561 36.1716 31.1424 31.4853Z"
+                        stroke="#737373" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M19.1211 26.5356L26.1922 19.4646" stroke="#737373" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M19.1211 19.4644L26.1922 26.5354" stroke="#737373" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+               </svg>
+               <div className={styles.wrapperInner}
+                    style={{ height: parsedValuesFromUpload.length ? "100%" : "", paddingBottom: "6rem" }}>
+                  <div className={styles.top}>
+                     <Heading h2 modal success
+                              style={{ marginBottom: "0" }}>Success!</Heading>
+                  </div>
                </div>
-               {
-                  !!parsedValuesFromUpload.length
-                  && <>
-                     <div className={styles.middle}>
-                        {parsedValuesFromUpload.map((value, index) => (
-                           <div key={value} className={styles.row}>
-                              <Input readOnly text inputName={value}
-                                     placeholder={value}
-                                     value={value}/>
-                              <FileUploadDropdown key={value} handleOption={handleOption}
-                                                  resetDropdown={resetDropdown} valueIndex={index}/>
-                           </div>
-                        ))}
-                     </div>
-                     <Button blue onClick={handleSubmitMapping}>
-                        <svg fill="none" height="32" viewBox="0 0 32 32"
-                             width="32" xmlns="http://www.w3.org/2000/svg">
-                           <path d="M12 26H9C7.14348 26 5.36301 25.2625 4.05025 23.9497C2.7375 22.637 2 20.8565 2 19C2 17.1435 2.7375 15.363 4.05025 14.0503C5.36301 12.7375 7.14348 12 9 12C9.58566 11.9998 10.1692 12.0711 10.7375 12.2125"
-                              stroke="white" strokeLinecap="round"
-                              strokeLinejoin="round" strokeWidth="1.5"/>
-                           <path d="M10 16C10 14.4155 10.3765 12.8536 11.0986 11.4432C11.8206 10.0327 12.8675 8.81406 14.1529 7.88758C15.4383 6.96109 16.9255 6.35333 18.4919 6.11437C20.0583 5.87541 21.6591 6.0121 23.1623 6.51317C24.6655 7.01424 26.0281 7.86534 27.1378 8.99635C28.2476 10.1274 29.0727 11.5059 29.5451 13.0183C30.0176 14.5308 30.1239 16.1338 29.8552 17.6954C29.5866 19.257 28.9507 20.7324 28 22"
-                              stroke="white" strokeLinecap="round"
-                              strokeLinejoin="round" strokeWidth="1.5"/>
-                           <path d="M14.7617 20.2375L18.9992 16L23.2367 20.2375" stroke="white" strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="1.5"/>
-                           <path d="M19 26V16" stroke="white" strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="1.5"/>
-                        </svg>
-                        <span>Upload Now</span>
-                     </Button>
-                  </>
-               }
             </div>
          </div>
-      </div>
+         : <div className={styles.modal}>
+            <div ref={outsideClickRef} className={styles.wrapper}
+                 style={{ height: parsedValuesFromUpload.length ? "90%" : "" }}>
+               <svg className={styles.close} fill="none" height="46"
+                    viewBox="0 0 46 46"
+                    width="46"
+                    xmlns="http://www.w3.org/2000/svg" onClick={closeModal}>
+                  <path d="M31.1424 31.4853C35.8287 26.799 35.8287 19.201 31.1424 14.5147C26.4561 9.82843 18.8581 9.82843 14.1718 14.5147C9.48551 19.201 9.48551 26.799 14.1718 31.4853C18.8581 36.1716 26.4561 36.1716 31.1424 31.4853Z"
+                        stroke="#737373" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M19.1211 26.5356L26.1922 19.4646" stroke="#737373" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M19.1211 19.4644L26.1922 26.5354" stroke="#737373" strokeLinecap="round"
+                        strokeLinejoin="round" strokeWidth="1.5"/>
+               </svg>
+               <div className={styles.wrapperInner} style={{ height: parsedValuesFromUpload.length ? "100%" : "" }}>
+                  <div className={styles.top}>
+                     <Heading blue h2 modal>Multi-Upload</Heading>
+                     <Text modal>You have confirmed the list of credentials
+                        are ready to be authenticated.</Text>
+                     <Input fileUpload fileName={fileName} inputName="csvUploader"
+                            isFileUploaded={!!parsedValuesFromUpload.length}
+                            onChange={uploadFileToClient}/>
+                     {fileUploadModalError && <Text error>{fileUploadModalError}</Text>}
+                  </div>
+                  {
+                     !!parsedValuesFromUpload.length
+                     && <>
+                        <div className={styles.middle}>
+                           {parsedValuesFromUpload.map((value, index) => (
+                              <div key={value} className={styles.row}>
+                                 <Input readOnly text inputName={value}
+                                        placeholder={value}
+                                        value={value}/>
+                                 <FileUploadDropdown key={value} handleOption={handleOption}
+                                                     resetDropdown={resetDropdown} valueIndex={index}/>
+                              </div>
+                           ))}
+                        </div>
+                        <Button blue onClick={handleSubmitMapping}>
+                           <svg fill="none" height="32" viewBox="0 0 32 32"
+                                width="32" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M12 26H9C7.14348 26 5.36301 25.2625 4.05025 23.9497C2.7375 22.637 2 20.8565 2 19C2 17.1435 2.7375 15.363 4.05025 14.0503C5.36301 12.7375 7.14348 12 9 12C9.58566 11.9998 10.1692 12.0711 10.7375 12.2125"
+                                    stroke="white" strokeLinecap="round"
+                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                              <path d="M10 16C10 14.4155 10.3765 12.8536 11.0986 11.4432C11.8206 10.0327 12.8675 8.81406 14.1529 7.88758C15.4383 6.96109 16.9255 6.35333 18.4919 6.11437C20.0583 5.87541 21.6591 6.0121 23.1623 6.51317C24.6655 7.01424 26.0281 7.86534 27.1378 8.99635C28.2476 10.1274 29.0727 11.5059 29.5451 13.0183C30.0176 14.5308 30.1239 16.1338 29.8552 17.6954C29.5866 19.257 28.9507 20.7324 28 22"
+                                    stroke="white" strokeLinecap="round"
+                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                              <path d="M14.7617 20.2375L18.9992 16L23.2367 20.2375" stroke="white" strokeLinecap="round"
+                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                              <path d="M19 26V16" stroke="white" strokeLinecap="round"
+                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                           </svg>
+                           <span>Upload Now</span>
+                        </Button>
+                     </>
+                  }
+               </div>
+            </div>
+         </div>
    )
 }
 
