cfc9d9f677d878dc808630f0d7208f79e6be3b61
AK <kkucenko.aleksejs@gmail.com>
2022-05-20 20:10:22 +0300

    name change on otp.service

--- qk-processing/test/unit/auth/otp.service.unit.spec.ts
+++ qk-processing/test/unit/auth/otp.service.unit.spec.ts
@@ -13,7 +13,7 @@ describe("OtpService Unit Test", () => {
   let prismaService: PrismaService;
   let ses: AwsSesService;
   
-  const otpMockFindUnique: OneTimePassword = {
+  const otpMock: OneTimePassword = {
     uuid: "baa3261d-b36a-4d5d-9f26-5fce27321df8",
     code: "3540",
     validUntil: new Date(1752996087 * 1000),
@@ -29,7 +29,7 @@ describe("OtpService Unit Test", () => {
   
   const userMockFindUnique: User = {
     uuid: "e7c66694-648e-4195-9558-b27b1a061512",
-    email: "a@k.lv",
+    email: "email@email.com",
     password: "123",
     role: "STUDENT",
     createdAt: new Date(1652991260 * 1000),
@@ -46,8 +46,8 @@ describe("OtpService Unit Test", () => {
         provide: PrismaService,
         useValue: {
           oneTimePassword: {
-            findUnique: jest.fn().mockReturnValue(Promise.resolve(otpMockFindUnique)),
-            create: jest.fn().mockReturnValue(Promise.resolve(otpMockFindUnique)),
+            findUnique: jest.fn().mockReturnValue(Promise.resolve(otpMock)),
+            create: jest.fn().mockReturnValue(Promise.resolve(otpMock)),
             delete: jest.fn(),
           }, 
           user: { findUnique: jest.fn().mockReturnValue(Promise.resolve(userMockFindUnique)) },
@@ -74,13 +74,13 @@ describe("OtpService Unit Test", () => {
     it("Should send email and return OtpResponseDto", async () => {
       await service.sendOtp(userMockFindUnique.email, 1);
       expect(ses.sendOtpEmail).toBeCalledTimes(1);
-      expect(ses.sendOtpEmail).toBeCalledWith(userMockFindUnique.email, otpMockFindUnique.code);
+      expect(ses.sendOtpEmail).toBeCalledWith(userMockFindUnique.email, otpMock.code);
       expect(await service.sendOtp(userMockFindUnique.email, 1)).toEqual(
-        new OtpResponseDto(otpMockFindUnique.uuid, otpMockFindUnique.validUntil, otpMockFindUnique.canBeResentAt),
+        new OtpResponseDto(otpMock.uuid, otpMock.validUntil, otpMock.canBeResentAt),
       );
     });
 
-    it("Should throw UserNotFoundException", () => {
+    it("Should throw UserNotFoundException if user not found", () => {
       const mockPrismaFindUnique = jest.fn().mockReturnValue(Promise.resolve(null));
       jest
         .spyOn(prismaService.user, "findUnique")
@@ -95,22 +95,22 @@ describe("OtpService Unit Test", () => {
   describe("checkOtp() - unit", () => {
 
     it("Should check if code is correct", async () => {
-      await service.checkOtp(otpMockFindUnique.code, otpMockFindUnique.uuid);
+      await service.checkOtp(otpMock.code, otpMock.uuid);
       expect(prismaService.oneTimePassword.delete).toBeCalledTimes(1);
     });
 
-    it("Should throw NotFoundException", () => {
+    it("Should throw NotFoundException if code not found", () => {
       const mockPrismaFindUnique = jest.fn().mockReturnValue(Promise.resolve(null));
       jest
         .spyOn(prismaService.oneTimePassword, "findUnique")
         .mockImplementation(mockPrismaFindUnique);
 
-      expect(async () => await service.checkOtp(otpMockFindUnique.code, otpMockFindUnique.uuid)).rejects.toThrowError(
+      expect(async () => await service.checkOtp(otpMock.code, otpMock.uuid)).rejects.toThrowError(
         new NotFoundException("code not found"),
       );
     });
 
-    it("Should throw GoneException", () => {
+    it("Should throw GoneException if code expired", () => {
       const mockPrismaFindUnique = jest.fn().mockReturnValue(Promise.resolve(otpMockExpired));
       jest
         .spyOn(prismaService.oneTimePassword, "findUnique")
@@ -121,8 +121,8 @@ describe("OtpService Unit Test", () => {
       );
     });
 
-    it("Should throw UnprocessableEntityException", () => {
-      expect(async () => await service.checkOtp("0000", otpMockFindUnique.uuid)).rejects.toThrowError(
+    it("Should throw UnprocessableEntityException if code incorrect", () => {
+      expect(async () => await service.checkOtp("0000", otpMock.uuid)).rejects.toThrowError(
         new UnprocessableEntityException("incorrect code"),
       );
     });
