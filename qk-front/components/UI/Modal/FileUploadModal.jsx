5ee61cd8e3627c692d9d558d6471c5d79c6d2509
AK <kkucenko.aleksejs@gmail.com>
2022-06-01 21:40:17 +0300

    fileupload modal mobile full

--- qk-front/components/UI/Modal/FileUploadModal.jsx
+++ qk-front/components/UI/Modal/FileUploadModal.jsx
@@ -31,9 +31,9 @@ const FileUploadModal = () => {
    const [fileName, setFileName] = useRecoilState(filenameState)
    const [parsedValuesFromUpload, setParsedValuesFromUpload] = useState([])
    const [mappingToValues, setMappingToValues] = useState([])
-   const [uploadSuccess, setUploadSuccess] = useState(true)
+   const [uploadSuccess, setUploadSuccess] = useState(false)
    const [loading, setLoading] = useState(false)
-   const [step, setStep] = useState(3)
+   const [step, setStep] = useState(1)
 
    //TODO: Make active field in dropdown according to figma ui — BLUE?
 
