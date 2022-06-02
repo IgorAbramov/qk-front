70a26b75ee819187512549f4ee0f2a6dc02d6a4e
AK <kkucenko.aleksejs@gmail.com>
2022-06-02 21:28:43 +0300

    otp resend

--- qk-front/components/AuthForms/FormTypes/TwoFactorForm.jsx
+++ qk-front/components/AuthForms/FormTypes/TwoFactorForm.jsx
@@ -33,8 +33,8 @@ const TwoFactorForm = ({ canBeResendAt, forgotPassword }) => {
    /**
     * Countdown setter logic.
     **/
-   const timerRef = useRef(0)
-   const timerCallback = useCallback(() => {
+   let timerRef = useRef(0)
+   let timerCallback = useCallback(() => {
       setDuration(calculateDuration(canBeResendAt))
       setHideResendButton(false)
    }, [canBeResendAt])
@@ -91,6 +91,17 @@ const TwoFactorForm = ({ canBeResendAt, forgotPassword }) => {
    }
 
    /**
+    * Resend code handler
+    */
+   const handleResendCode = async () => {
+      await axios.post(`${processingUrl}/auth/otp`, { email: formData.email })
+         .then(response => {
+            console.log(response)
+         })
+         .catch(error => console.log(error))
+   }
+
+   /**
     * Countdown updater logic.
     **/
    useEffect(() => {
@@ -127,7 +138,7 @@ const TwoFactorForm = ({ canBeResendAt, forgotPassword }) => {
                {
                   hideResendButton ? <Text transparent>-</Text>
                      : duration.minutes() === 0 && duration.seconds() === 0
-                        ? <Text grey>Resend code</Text>
+                        ? <Text grey onClick={handleResendCode}>Resend code</Text>
                         : <Text grey>{`${duration.minutes()}:${duration.seconds() < 10 ? "0" + duration.seconds() : duration.seconds()}`}</Text>
                }
             </form>
