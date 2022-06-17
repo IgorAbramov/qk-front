823b2a0913f0cbebc2a201bbb9a814cc7c985c82
AK <kkucenko.aleksejs@gmail.com>
2022-06-17 14:38:31 +0300

    add share page forbidden logic

--- qk-front/components/DashboardItem/SharedCredentialsItem.jsx
+++ qk-front/components/DashboardItem/SharedCredentialsItem.jsx
@@ -9,36 +9,7 @@ import Button from "../UI/Button/Button"
 import Text from "../UI/Text/Text"
 import styles from "./DashboardItem.module.scss"
 
-const data = {
-   uuid: "3e617da7-bfb7-4a16-8721-13316a51389b",
-   did: "did:hedera:testnet:FEAQXNWAZNWVMysDERXv5wXT75SGdLEHANsKuZdGctxU;hedera:testnet:fid=0.0.34921762;hedera:testnet:tid=0.0.34921760",
-   status: "ACTIVATED",
-   studentUuid: "bf4fe6f3-f5a1-4794-aced-4deb4ba8c67a",
-   institutionUuid: "89f43c8f-1434-4caf-92bd-990b3f112da1",
-   uploadUuid: "bc379cfe-257a-4ee9-94b4-30c79f007300",
-   certificateId: null,
-   graduatedName: "Dylan Bomstein",
-   qualificationName: "Bachelors of Science in Business",
-   majors: null,
-   minors: null,
-   awardingInstitution: "Learning Institute",
-   qualificationLevel: "Bachelors",
-   awardLevel: "",
-   studyLanguage: "English",
-   info: "None",
-   gpaFinalGrade: "",
-   studyStartedAt: "2013-08-05T00:00:00.000Z",
-   studyEndedAt: "2017-05-05T00:00:00.000Z",
-   graduatedAt: "2017-05-05T10:00:00.000Z",
-   expiresAt: "2021-12-03T10:00:00.000Z",
-   authenticatedAt: "2022-06-13T13:45:42.957Z",
-   authenticatedBy: "Institution Institution",
-   authenticatedTitle: null,
-   createdAt: "2022-06-13T13:45:49.933Z",
-   updatedAt: "2022-06-13T14:05:27.277Z"
-}
-
-const SharedCredentialsItem = () => {
+const SharedCredentialsItem = ({ data }) => {
    
    const [showData, setShowData] = useState(false)
 
